f025f3f01b6f4f886fd836e51f2917bcf08243dd
Vinicius <viniciusr.camargo@gmail.com>
2024-11-07 10:32:57 -0300

    Resolução dos exercícios em JS

---
+++ 14112024 - Resolucao dos exercicios/exerciciosJS/exercicio3.js
@@ -0,0 +1,9 @@
+let numero = 0;
+
+if(numero > 0){
+    console.log('O número ' + numero + ' é positivo.')
+}else if(numero < 0){
+    console.log('O número ' + numero + ' é negativo.')
+}else{
+    console.log('O número ' + numero + ' é um zerão.')
+}
