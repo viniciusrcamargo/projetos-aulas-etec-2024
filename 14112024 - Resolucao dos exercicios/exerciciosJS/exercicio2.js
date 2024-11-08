1128e5e6c10bfbf628b7f8c962a214009e66b544
Vinicius <viniciusr.camargo@gmail.com>
2024-11-08 10:31:31 -0300

    Resolução dos exercicíos em JS

---
+++ 14112024 - Resolucao dos exercicios/exerciciosJS/exercicio2.js
@@ -0,0 +1,8 @@
+let num1 = 90;
+let num2 = 44;
+//if é igual a se
+if(num1 > num2){//verificação ou condicional
+    console.log('O número ' + num1 + ' é maior!');
+}else{//igual a senão
+    console.log('O número ' + num2 + ' é maior');
+}
