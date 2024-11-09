12c997c9f16fd0b6a358739392bebbd82fd29b8a
Vinicius <viniciusr.camargo@gmail.com>
2024-11-09 10:28:02 -0300

    Resolução dos exercícios em JS

---
+++ 14112024 - Resolucao dos exercicios/exerciciosJS/exercicio1.js
@@ -0,0 +1,7 @@
+let nome = 'José Raimundo';
+let idade = 89;
+let cursando = 'Desenvolvimento de Sistemas'
+
+console.log(`Meu nome é ${nome} tenho ${idade} anos e estou cursando  ${cursando}`);//template string
+//console.log('Meu nome é ' + nome + ' tenho ' + idade + ' anos e estou cursando ' + cursando);
+//concatenar é juntar valores 
