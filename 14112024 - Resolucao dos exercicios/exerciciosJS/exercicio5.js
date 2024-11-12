e5b2016dce4e0496b62dcbccd36e65326604823a
Vinicius <viniciusr.camargo@gmail.com>
2024-11-12 10:36:03 -0300

    Resolução dos exercícios em JS

---
+++ 14112024 - Resolucao dos exercicios/exerciciosJS/exercicio5.js
@@ -0,0 +1,10 @@
+let nota1 = 6
+let nota2 = 6
+let nota3 = 6
+const media = (nota1 + nota2 + nota3) / 3;
+
+if(media >= 7){
+    console.log('Passou de ano! média ' + media.toFixed(2));
+}else{
+    console.log('sua média foi '+ media.toFixed(2) + ' Vai precisar ficar até dia 17 de dezembro estudando, fica a dica!')
+}
