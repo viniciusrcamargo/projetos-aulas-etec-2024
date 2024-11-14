2d8f38f1f3129273ad310e1471eb85e971488baa
Vinicius <viniciusr.camargo@gmail.com>
2024-11-14 10:39:02 -0300

    Resolução dos exercícios em JS

---
+++ 14112024 - Resolucao dos exercicios/exerciciosJS/exercicio8.js
@@ -0,0 +1,14 @@
+let numA = 20;
+let numB = 40;
+let soma =  numA + numB
+let sub = numA - numB
+let mult = numA * numB
+let div = numA / numB
+console.log('A soma é ' + soma)
+console.log('A subtração é ' + sub)
+console.log('A multiplicação é ' + mult)
+if(numB === 0){
+    console.log('Não é permitido divisão por 0')
+}else{
+    console.log('A divisão é ' + div)
+}
