4d148bcee581d58a9b6bd9f8c83754ce603f81c0
Vinicius <viniciusr.camargo@gmail.com>
2024-11-17 20:20:43 -0300

    Entendendo Arrays

---
+++ 16112024 - Entendendo arrays/doWhile.js
@@ -0,0 +1,8 @@
+const carros = ['HRV', 'HB20', 'Fusca', 'Opala', 'Mustang', '147 fiat'];
+
+let i = 0;
+
+do{
+    console.log("O carro do ano 202" + i + " é o " + carros[i]);
+    i++
+}while(i < carros.length)//condição
