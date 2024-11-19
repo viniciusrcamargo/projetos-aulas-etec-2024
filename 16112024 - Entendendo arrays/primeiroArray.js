c8d379ae2e658c373d65791e17500432adfe031f
Vinicius <viniciusr.camargo@gmail.com>
2024-11-19 20:21:48 -0300

    Entendendo Arrays

---
+++ 16112024 - Entendendo arrays/primeiroArray.js
@@ -0,0 +1,8 @@
+let animais =  ['Totó', 'Neguinho', 'Bebel', 'Lulu'];
+// console.log('Meu primeiro cachorro foi ' + animais[3]);
+/*
+console.log(animais.length)
+*/
+for(var i = 0; i < animais.length; i++){
+    console.log('Eu tenho um cachorro chamado(a) ' + animais[i]);
+}
