1eb6602f8fe1cafc893f16e6ec1b5d4526cc70cc
Vinicius <viniciusr.camargo@gmail.com>
2024-11-20 20:22:17 -0300

    Entendendo Arrays

---
+++ 16112024 - Entendendo arrays/switch.js
@@ -0,0 +1,21 @@
+let nota = 5;
+
+switch (nota){
+    case 6:
+        console.log('Você tirou 6, tente denovo!');
+        break;//parada
+    case 7:
+        console.log('Você tirou 7, tente denovo!');
+        break;
+    case 8:
+        console.log('Você tirou 8, tente denovo!');
+        break;
+    case 9:
+        console.log('Você tirou 6, tente denovo!');
+        break;
+    case 10:
+        console.log('Parabéns! Você foi aprovado na Usp!');
+        break;
+    default://padrão
+        console.log('Nota não informada')
+}
