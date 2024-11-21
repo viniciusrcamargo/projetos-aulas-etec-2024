169b9d4d54515ae4e20c2860c997adea75dad54b
Vinicius <viniciusr.camargo@gmail.com>
2024-11-21 20:22:49 -0300

    Entendendo Arrays

---
+++ 16112024 - Entendendo arrays/while.js
@@ -0,0 +1,7 @@
+const alunos = ['Davi', 'Henrique', 'Everton', 'Matheus'];
+
+let i = 0;//variável contadora //while significa enquanto
+while(i < alunos.length){//length tamanho total do array
+    console.log("Aluno da Etec - " + alunos[i]);
+    i++;//incrementa mais 1
+}
